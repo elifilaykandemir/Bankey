7fda20b6b237951b3a3d5b7642069f1f6b9a1d17
elifilaykandemir <elif.ilay.kndmr@gmail.com>
2023-11-21 14:14:43 +0300

    Added first onboarding scene

---
+++ Onboarding/OnboardingViewController.swift
@@ -0,0 +1,61 @@
+//
+//  OnboardingViewController.swift
+//  Bankey
+//
+//  Created by Elif İlay KANDEMİR on 21.11.2023.
+//
+
+import UIKit
+
+class OnbordingViewController: UIViewController {
+    
+    lazy var stackView : UIStackView = {
+        var stackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        stackView.spacing = 20
+        return stackView
+    }()
+    
+    lazy var imageView : UIImageView = {
+        var imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = .scaleAspectFit
+        imageView.image = UIImage(named: "delorean")
+        return imageView
+    }()
+    
+    lazy var label : UILabel = {
+        var label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont.preferredFont(forTextStyle: .title3)
+        label.textAlignment = .center
+        label.adjustsFontForContentSizeCategory = true
+        label.numberOfLines = 0
+        label.text = " Bankey is faster, eaiser to use and has a brand new look and feel that will make you feel like you are back in 1989"
+        return label
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setUpLayout()
+    }
+}
+
+extension OnbordingViewController {
+    
+    func setUpLayout(){
+        stackView.addArrangedSubview(imageView)
+        stackView.addArrangedSubview(label)
+        
+        view.addSubview(stackView)
+        
+        NSLayoutConstraint.activate([
+            stackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            stackView.leadingAnchor.constraint(equalToSystemSpacingAfter: view.leadingAnchor, multiplier: 1),
+            view.trailingAnchor.constraint(equalToSystemSpacingAfter: stackView.trailingAnchor, multiplier: 1)
+        ])
+    }
+   
+}
