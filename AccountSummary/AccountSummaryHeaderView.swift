c689fa56ca6e9823a348d74dd3556ef4960f67f8
elifilaykandemir <elif.ilay.kndmr@gmail.com>
2024-01-03 23:24:43 +0300

    feature: Style header view

--- AccountSummary/AccountSummaryHeaderView.swift
+++ AccountSummary/AccountSummaryHeaderView.swift
@@ -17,23 +17,23 @@ class AccountSummaryHeaderView: UIView {
             commonInit()
         }
         
-        required init?(coder aDecoder: NSCoder) {
-            super.init(coder: aDecoder)
-            commonInit()
-        }
-        
-        override var intrinsicContentSize: CGSize {
-            return CGSize(width: UIView.noIntrinsicMetric, height: 144)
-        }
-        
-        private func commonInit() {
-            
-            let bundle = Bundle(for: AccountSummaryHeaderView.self)
-            bundle.loadNibNamed("AccountSummaryHeaderView", owner: self, options: nil)
-            addSubview(contentView)
-            contentView.backgroundColor = appColor
-            
-            contentView.setUpConstraints(leadingAnchor: self.leadingAnchor,topAnchor: self.topAnchor,trailingAnchor: self.trailingAnchor,bottomAnchor: self.bottomAnchor)
-            
-        }
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        commonInit()
     }
+    
+    override var intrinsicContentSize: CGSize {
+        return CGSize(width: UIView.noIntrinsicMetric, height: 144)
+    }
+    
+    private func commonInit() {
+        
+        let bundle = Bundle(for: AccountSummaryHeaderView.self)
+        bundle.loadNibNamed("AccountSummaryHeaderView", owner: self, options: nil)
+        addSubview(contentView)
+        contentView.backgroundColor = appColor
+        
+        contentView.setUpConstraints(leadingAnchor: self.leadingAnchor,topAnchor: self.topAnchor,trailingAnchor: self.trailingAnchor,bottomAnchor: self.bottomAnchor)
+        
+    }
+}
