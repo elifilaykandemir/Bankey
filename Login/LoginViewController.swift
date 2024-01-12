c97e007fed5ba77e3d95b66cf7e93bfe5ed128dd
elifilaykandemir <elif.ilay.kndmr@gmail.com>
2024-01-12 11:09:04 +0300

    Title and subtitle is added.

--- Login/LoginViewController.swift
+++ Login/LoginViewController.swift
@@ -22,6 +22,27 @@ class LoginViewController: UIViewController {
     
     let loginView = LoginView()
     
+    let titleLabel: UILabel = {
+        var label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textAlignment = .center
+        label.font = UIFont.preferredFont(forTextStyle: .largeTitle)
+        label.adjustsFontForContentSizeCategory = true
+        label.text = "Bankey"
+        return label
+    }()
+    
+    let subtitleLabel: UILabel = {
+        var label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textAlignment = .center
+        label.font = UIFont.preferredFont(forTextStyle: .title3)
+        label.adjustsFontForContentSizeCategory = true
+        label.numberOfLines = 0
+        label.text = "Your premium source for all things banking!"
+        return label
+    }()
+    
     lazy var signInButton : UIButton = {
         var button = UIButton(type: .system)
         button.translatesAutoresizingMaskIntoConstraints = false
@@ -72,15 +93,21 @@ extension LoginViewController {
     }
     
     private func configure() {
+        view.addSubview(titleLabel)
+        view.addSubview(subtitleLabel)
         view.addSubview(loginView)
         view.addSubview(signInButton)
         view.addSubview(errorMessageLabel)
-     
         
         loginView.setUpConstraints(leadingAnchor: view.leadingAnchor, leadingConstant: 20,
                                    trailingAnchor: view.trailingAnchor, trailingConstant: 14,
                                    centerYAnchor: view.centerYAnchor, centerXAnchor: view.centerXAnchor
         )
+        titleLabel.setUpConstraints(centerXAnchor:view.centerXAnchor)
+        
+        subtitleLabel.setUpConstraints(leadingAnchor:loginView.leadingAnchor,topAnchor: titleLabel.bottomAnchor, topConstant: 24, trailingAnchor:loginView.trailingAnchor)
+        loginView.setUpConstraints(topAnchor: subtitleLabel.bottomAnchor, topConstant: 24)
+    
     
         signInButton.setUpConstraints(leadingAnchor: view.leadingAnchor, leadingConstant: 20,
                                       topAnchor:loginView.bottomAnchor, topConstant: 16,
@@ -123,4 +150,19 @@ extension LoginViewController {
         errorMessageLabel.isHidden = false
         errorMessageLabel.text = message
     }
+    
+    override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+        super.traitCollectionDidChange(previousTraitCollection)
+        if self.traitCollection.userInterfaceStyle == .dark {
+            // Set colors for dark mode
+            titleLabel.textColor = UIColor.white
+            subtitleLabel.textColor = UIColor.white
+            
+        } else {
+            // Set colors for light mode
+            titleLabel.textColor = UIColor.white
+            subtitleLabel.textColor = UIColor.white
+        }
+    }
+
 }
